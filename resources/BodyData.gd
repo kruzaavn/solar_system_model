3453fc4b685cc56715a4a23f4bc1bdfe242abfa8
kruzaavn <aleks.kruza@gmail.com>
2023-04-11 13:12:17 -0400

    made body resources local to scene

--- resources/BodyData.gd
+++ resources/BodyData.gd
@@ -3,22 +3,44 @@ extends Resource
 
 class_name BodyData
 
-@export var noise: NoiseTexture2D
-
-@export var gradient: GradientTexture1D
-
-@export_enum("ROCKY", "ATMOSPHERE", "OCEANIC", "GAS_GIANT") var biome: int :
+@export var noise: NoiseTexture2D:
 	set(value):
 		
+		noise = value
+		emit_changed()
+
+@export var gradient: GradientTexture1D:
+	set(value):
+		
+		gradient = value
+		emit_changed()
+
+@export_enum("ROCKY", "ATMOSPHERE", "OCEANIC", "GAS_GIANT") var biome: int:
+	set(value):
 		biome = value
 		emit_changed()
 
-@export_range(1.0, 10.0, 0.01) var scale: float = 1.0
+@export_range(1.0, 10.0, 0.01) var scale: float = 1.0:
+	set(value):
+		scale = value
+		emit_changed()
 
-@export var ocean: Color
+@export var ocean: Color:
+	set(value):
+		ocean = value
+		emit_changed()
 
-@export var ocean_fresnel: Color
+@export var ocean_fresnel: Color:
+	set(value):
+		ocean_fresnel = value
+		emit_changed()
 
-@export var atmosphere: Color
+@export var atmosphere: Color:
+	set(value):
+		atmosphere = value
+		emit_changed()
 
-@export var atmosphere_fresnel: Color
+@export var atmosphere_fresnel: Color:
+	set(value):
+		atmosphere_fresnel = value
+		emit_changed()
